c0a2549b14595f1c9c2fd9e21a69cfa64b5bd8c0
JhonLlanes <jlc_rb@live.com>
2026-04-14 23:08:03 -0500

    feat(ui): first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <>
+     <div className="bg-amber-50 h-screen w-screen flex items-center justify-center "> 
+
+       <span className="text-2xl font-bold"> GRUPO 24 </span>
+     </div>
+    </>
+  )
+}
+export default App
